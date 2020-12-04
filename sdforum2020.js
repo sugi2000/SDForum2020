aab083bbe526a27b028b44121eac76ec1838e15f
Shohei Yokoyama <1260486+abarth500@users.noreply.github.com>
2020-12-04 16:57:45 +0900

    image cover for labo video

---
+++ sdforum2020.js
@@ -0,0 +1,178 @@
+let videoJson = 'videos.json';
+let videos = {};
+let items = {};
+let player = {};
+let opened = {};
+let viewed = {};
+let ready = false;
+let originURL = location.protocol + '//' + location.hostname + (location.port != "" ? ":" + location.port : "");
+/*document.addEventListener("DOMContentLoaded", () => {*/
+console.log("DOMContentLoaded");
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+function onYouTubeIframeAPIReady() {
+    console.log("onYouTubeIframeAPIReady");
+    fetch(videoJson)
+        .then(response => response.json())
+        .then(data => {
+            videos = data.depertment;
+            items = data.faculty;
+            let showLaboVideo = (faculty) => {
+                videos[faculty].forEach((v) => {
+                    console.log("V YOUTUBE:", v.youtube);
+                    if (v.youtube == "") { return; }
+                    let tpl = document.getElementById('card-template').querySelector('ui').cloneNode(true);
+                    tpl.querySelector('.labo-video').id = v.youtube;
+                    tpl.querySelector('.labo-video').style.backgroundImage = "url(https://img.youtube.com/vi/" + v.youtube + "/sddefault.jpg)";
+                    tpl.querySelector('.labo-video').addEventListener('click', (e) => {
+                        e.stopPropagation();
+                        console.log('Click:', e.currentTarget.id);
+                        if (!viewed.hasOwnProperty(e.currentTarget.id)) {
+                            viewed[e.currentTarget.id] = new YT.Player(e.currentTarget.id, {
+                                videoId: e.currentTarget.id,
+                                width: "600px",
+                                playerVars: {
+                                    rel: 0,
+                                    'enablejsapi': 1,
+                                    'origin': originURL
+                                },
+                                events: {
+                                    'onReady': (e) => {
+                                        e.target.playVideo();
+                                    }
+                                }
+                            });
+                        }
+
+                    });
+                    tpl.querySelector('.labo-name').innerText = v.name;
+                    tpl.querySelector('.labo-position').innerText = v.position;
+                    tpl.querySelector('.labo-title').innerText = v.title;
+                    tpl.querySelector('.labo-keyword').innerText = v.keyword;
+                    tpl.querySelector('.labo-face').src = v.picture;
+                    document.querySelector('section.' + faculty + ' ul.labos').appendChild(tpl);
+                    let originURL = location.protocol + '//' + location.hostname + (location.port != "" ? ":" + location.port : "");
+                    console.log(originURL);
+                    /*
+                    let labop = new YT.Player(v.youtube, {
+                        videoId: v.youtube,
+                        width: "1024px",
+                        playerVars: {
+                            rel: 0,
+                            'enablejsapi': 1,
+                            'origin': originURL
+                        }
+                    });*/
+                });
+            }
+            let showVideo = (faculty, ignoreLaboVideo = false) => {
+                document.querySelectorAll('.menuitem').forEach((ele) => { ele.classList.add('kurukuru') });
+                let originURL = location.protocol + '//' + location.hostname + (location.port != "" ? ":" + location.port : "");
+                player[faculty] = new YT.Player('player_' + faculty, {
+                    videoId: items[faculty],
+                    width: "600px",
+                    playerVars: {
+                        rel: 0,
+                        'enablejsapi': 1,
+                        'origin': originURL
+                    }, events: {
+                        'onReady': () => {
+                            console.log("ready!!!", ignoreLaboVideo);
+                            document.querySelectorAll('.menuitem').forEach((ele) => { ele.classList.remove('kurukuru') });
+                            if (!ignoreLaboVideo && !opened.hasOwnProperty(faculty) && videos.hasOwnProperty(faculty)) {
+                                showLaboVideo(faculty);
+                                opened[faculty] = true;
+                            }
+                        }
+                    }
+                });
+            }
+            let defFaculty = (location.hash == "") ? "sd" : location.hash.replace(/^\#/, '');
+            document.querySelector('.' + defFaculty + '[type=radio]').checked = true;
+            Object.keys(items).forEach((faculty) => {
+                showVideo(faculty, defFaculty != faculty);
+            });
+            ready = true;
+            document.querySelector('.video').addEventListener('click', (e) => {
+                e.stopPropagation();
+                document.querySelector('#mode').checked = !document.querySelector('#mode').checked;
+                if (document.querySelector('#mode').checked) {
+                    Object.keys(player).forEach((p) => {
+                        player[p].pauseVideo();
+                    });
+                }
+                Object.keys(items).forEach((p) => {
+                    if (document.querySelector('.videoselector.' + p).checked) {
+                        if (document.querySelector('#mode').checked) {
+                            player[p].pauseVideo();
+                        } else {
+                            player[p].playVideo();
+                        }
+                    }
+                });
+            }, false);
+            document.querySelectorAll('.menuitem').forEach((ele) => {
+                ele.addEventListener('click', function (e) {
+                    console.log("menu click:", e);
+                    e.stopPropagation();
+                    let faculty = this.getAttribute('x-item');
+                    console.log(faculty);
+                    if (!opened.hasOwnProperty(faculty) && videos.hasOwnProperty(faculty)) {
+                        showLaboVideo(faculty);
+                        opened[faculty] = true;
+                    }
+                    document.querySelector('.' + faculty + '[type=radio]').checked = true;
+                    Object.keys(items).forEach(p => {
+                        if (player.hasOwnProperty(p)) {
+                            console.log("pause " + p);
+                            console.log(player[p]);
+                            player[p].pauseVideo();
+                        }
+                    });
+                    history.pushState('', '', "#" + faculty);
+                    //if (ready && !player.hasOwnProperty(faculty)) {
+                    //    showVideo(faculty);
+                    //} else {
+                    //    document.querySelector('.' + faculty + '[type=radio]').checked = true;
+                    //}
+                    /*
+                    if (videos[faculty] && document.querySelector('section.' + faculty + ' ul.labos')) {
+                        console.log("sort");
+                        videos[faculty].sort(
+                            function () {
+                                return Math.random() - 0.5;
+                            }
+                        );
+                        videos[faculty].forEach((v) => {
+                            console.log("V YOUTUBE:", v.youtube);
+                            if (v.youtube == "") { return; }
+                            let tpl = document.getElementById('card-template').querySelector('ui').cloneNode(true);
+                            tpl.querySelector('.labo-video').id = v.youtube;
+                            tpl.querySelector('.labo-name').innerText = v.name;
+                            tpl.querySelector('.labo-position').innerText = v.position;
+                            tpl.querySelector('.labo-title').innerText = v.title;
+                            tpl.querySelector('.labo-keyword').innerText = v.keyword;
+                            tpl.querySelector('.labo-face').src = v.picture;
+                            document.querySelector('section.' + faculty + ' ul.labos').appendChild(tpl);
+                            let originURL = location.protocol + '//' + location.hostname + (location.port != "" ? ":" + location.port : "");
+                            console.log(originURL);
+                            let player = new YT.Player(v.youtube, {
+                                videoId: v.youtube,
+                                width: "1024px",
+                                playerVars: {
+                                    rel: 0,
+                                    'enablejsapi': 1,
+                                    'origin': originURL
+                                }
+                            });
+                        });
+                        videos[faculty] = [];
+                    }*/
+                });
+            }, false);
+
+        });
+}
+/* });*/
